be6edb6729a4db02761c6a5c9971a694d3c29a01
Jirachai C <antronic.inc@gmail.com>
2018-10-24 21:22:49 +0700

    Try to use room but disabled

--- server/index.js
+++ server/index.js
@@ -6,11 +6,21 @@ app.listen(3003, function() {
   console.log('socket is starting')
 })
 
+io.on('connection', function(socket) {
+  console.log('connect')
+})
+
+// let room = 0
 const staff = io.of('/staff')
   .on('connection', function(socket) {
     console.log('> staff is connected')
+    // socket.join('room_' + (++room))
 
-    socket.on('chat', function(msg) {
+    // console.log('room > ' + room)
+    // console.log('room- > ' + (room - 1))
+    // staff.to('room_' + (room - 1)).emit('message', 'has someone join')
+
+    socket.on('chat', function (msg) {
       console.log('s> ' + msg)
       staff.emit('message', msg)
     })
